616906101dec2d09744b902ee7fbe582a09aaf78
Ibon <ibicano@gmail.com>
2015-03-20 18:53:22 +0100

    Clases de instrucciones

--- ibines/CPU.py
+++ ibines/CPU.py
@@ -2,8 +2,61 @@
 
 class CPU:
 
+    ###########################################################################
+    # Métodos públicos
+    ###########################################################################
+
+    # Constructor
     def __init__(self):
         pass
 
-    #registro contador
-    __pc = 0x0000
+    # Leen los registros
+    def getRegPc(self):
+        return self.__rereg_pc
+
+    def getRegSp(self):
+        return self.__rereg_sp
+
+    def getRegA(self):
+        return self.__rereg_a
+
+    def getRegX(self):
+        return self.__rereg_x
+
+    def getRegY(self):
+        return self.__rereg_y
+
+    def getRegP(self):
+        return self.__rereg_p
+
+    # Escriben los registros
+    def setRegPc(self, r):
+        self.__rereg_pc = r
+
+    def setRegSp(self, r):
+        self.__rereg_sp = r
+
+    def setRegA(self, r):
+        self.__rereg_a = r
+
+    def setRegX(self, r):
+        self.__rereg_x = r
+
+    def setRegY(self, r):
+        self.__rereg_y = r
+
+    def setRegP(self, r):
+        self.__rereg_p = r
+
+
+    ###########################################################################
+    # Variables privadas
+    ###########################################################################
+
+    #registros
+    __reg_pc = 0x0000        # Program Counter (16-bit)
+    __reg_sp = 0x00          # Stack Pointer (8-bit)
+    __reg_a = 0x00           # Accumulator (8-bit)
+    __reg_x = 0x00           # Index X (8-bit)
+    __reg_y = 0x00           # Index Y (8-bit)
+    __reg_p = 0x00           # Processor Status (8-bit)
---
+++ ibines/Instruction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Instruction(object):
+
+    def __init__(self):
+        pass
+
+    def execute(self, cpu):
+        pass
+
+
+class BEQ(Instruction):
+
+    def __init__(self):
+        super(BEQ, self).__init__()
+
+
+
+
