db298c49ef4a1ad50e167389406aa58669e2c695
ibon <ibicano@yahoo.es>
2015-06-12 11:49:45 +0200

    Comentando código.

--- ibines/Memory.py
+++ ibines/Memory.py
@@ -2,11 +2,10 @@
 
 import Mapper
 
-"""
-Memory
-
-Descripción: Implementa la Memoria del sistema
-"""
+###############################################################################
+# Clase: NES
+# Descripción: Implementa la memoria principal de la NES
+###############################################################################
 class Memory(object):
 
     SIZE = 0x10000
@@ -14,66 +13,67 @@ class Memory(object):
     # Constructor
     # Se le pasa una instancia de la PPU y otra de la ROM para el mapeo en memoria de ambos
     def __init__(self, ppu, mapper, joypad_1):
-        #######################################################################
-        # Variables de instancia
-        #######################################################################
-
         # Array para almacenar el contenido de la memoria
         self._memory = [0x00] * Memory.SIZE
         self._joypad_1 = joypad_1
         self._ppu = ppu
         self._mapper = mapper
 
-        #######################################################################
-        #######################################################################
 
-
-    # Devuelve el contenido de una posición de memoria
+    ###############################################################################
+    # Función: read(addr)
+    # Parámetros:
+    #   addr -> dirección de memoria a leer (16 bits)
+    # Descripción: Lee la posición de memoria indicada por el parámetro 'addr', que
+    # tiene que se un número de 16 bits, y devuelve el contenido
+    ###############################################################################
     def read_data(self, addr):
         d = 0x00
-        if addr >= 0x8000:    # Lee del mapper de la ROM
+        if addr >= 0x8000:      # Memoria del programa. Lee del mapper de la ROM
             d = self._mapper.read_prg(addr)
-        elif addr >= 0x0000 and addr < 0x2000:
+        elif addr >= 0x0000 and addr < 0x2000:      # Memoria RAM
             d = self._memory[addr]
         elif addr >= 0x2000 and addr < 0x4000:     # Direcciones de los registros PPU
             d = self._ppu.read_reg(0x2000 + (addr & 0x07))
-        elif addr >= 0x4000 and addr < 0x4020:
-            if addr == 0x4016:
+        elif addr >= 0x4000 and addr < 0x4020:      # Más registros I/O
+            if addr == 0x4016:      # Registro del Joypad 1
                 d = self._joypad_1.read_reg()
-            elif addr == 0x4017:
+            elif addr == 0x4017:        # Registro del Joypad 2
                 pass
-        elif 0x6000 <= addr <= 0x7FFF:
+        elif 0x6000 <= addr <= 0x7FFF:      # Memoria de estado de la partida.
             d = self._memory[addr]
 
         return d
 
 
-    # TODO: acabar esta función
-    # Establece el contenido de una posición de memoria
-    # Se escribe en todas las posiciones de las que se hace mirror. Sería más
-    # eficiente no escribir todas y mapear las posiciones en una soloa
-    # OPTIMIZE: Lo expuesto anteriormente
+    ###############################################################################
+    # Función: write(data, addr)
+    # Parámetros:
+    #   data -> dato a escribir en memoria (8 bits)
+    #   addr -> dirección de memoria a escribir (16 bits)
+    # Descripción: Escribe el dato 'data' en la posición de memoria 'addr'
+    ###############################################################################
     def write_data(self, data, addr):
         d = data & 0xFF
         addr = addr & 0xFFFF
 
-        if addr >= 0x0000 and addr < 0x2000:
+        if addr >= 0x0000 and addr < 0x2000:        # Espacio de memoria RAM
             n = addr % 0x800
             self._memory[n] = d
             self._memory[0x0800 + n] = d
             self._memory[0x1000 + n] = d
             self._memory[0x1800 + n] = d
-        elif addr >= 0x2000 and addr < 0x4000: # Direcciones de los registros PPU
+        elif addr >= 0x2000 and addr < 0x4000:      # Direcciones de los registros PPU
             n = 0x2000 + (addr & 0x07)
             self._ppu.write_reg(d, n)
-        elif addr >= 0x4000 and addr <= 0x401F: # Más registros I/O
-            if addr == 0x4014:
+        elif addr >= 0x4000 and addr <= 0x401F:     # Más registros I/O
+            if addr == 0x4014:      # Escritura de memoria de Sprites por DMA
                 self._ppu.write_sprite_dma(self, d)
-            elif addr == 0x4016:
+            elif addr == 0x4016:        # Joypad 1
                 self._joypad_1.write_reg(d)
-            elif addr == 0x4017:
+            elif addr == 0x4017:        # Joypad 2
                 pass
         elif 0x6000 <= addr <= 0x7FFF:
             self._memory[addr] = d
-        elif addr >= 0x8000:
+        elif addr >= 0x8000:            # Memoria de programa. Escribe al mapper.
             self._mapper.write_prg(d, addr)
--- ibines/NES.py
+++ ibines/NES.py
@@ -15,44 +15,40 @@ import cProfile
 import pstats
 
 
-
+###############################################################################
+# Clase: NES
+# Descripción: Clase que implementa el sistema completo
+###############################################################################
 class NES(object):
 
     DEBUG = True
 
     def __init__(self, file_name):
-        #######################################################################
-        # Variables de instancia
-        #######################################################################
         self._rom = ROM(file_name)
         self._mapper = Mapper.instance_mapper(self._rom.get_mapper_code(), self._rom)
-
         self._ppu = PPU(self._mapper)
-
         self._joypad_1 = Joypad()
-
         self._memory = Memory(self._ppu, self._mapper, self._joypad_1)
-
         self._cpu = CPU(self._memory, self._ppu)
-
 
         # Registros I/O del  JoyPad
         self._reg_joypad_1 = 0x00       # Dirección 0x4016 - read/write
         self._reg_joypad_2 = 0x00       # Dirección 0x4017 - read/write
-        #######################################################################
-        #######################################################################
 
-
-    # Aquí se implementa el bucle principal de la NES. Cada iteración equivale a un
-    # ciclo de reloj, para más precisión y exactitud conceptual
+    ###############################################################################
+    # Función: run()
+    # Descripción: Aquí se implementa el bucle principal de la NES. Cada iteración
+    # ejecuta una instrucción llevando la cuenta de los ciclos de CPU consumidos
+    # para sincronizarse con la PPU
+    ###############################################################################
     def run(self):
-        stats_cycles = 0
-        stats_total_time = time.time()
-        total_cycles = 0
-        key_counter = 0
-        cycles = 0
+        stats_cycles = 0                    # Ciclos de CPU ejecutados para fines estadisticos
+        stats_total_time = time.time()      # Tiempo de ejecución transcurrido para fines estadísticos
+        total_cycles = 0                    # Ciclos totales de CPU desde que se inicia el emulador
+        key_counter = 0                     # Contador de ciclos hasta la siguiente comprobación de una pulsación de tecla
+        cycles = 0                          # Ciclos de CPU de una iteración del bucle
 
-
+        # Bucle principal
         while 1:
             # Si hay interrupciones y la CPU no está ocupada, las lanzamos
             if self._ppu.get_int_vblank():
@@ -67,6 +63,8 @@ class NES(object):
             # Restamos los ciclos de ejecución a la PPU
             self._ppu.exec_cycle(cycles)
 
+            # Aquí se detectan las pulsaciones en los dispositivos de entrada. Por cuestiones de rendimiento, ya que
+            # es bastante caro comprobar en cada iteración del bucle, se hace solo cada 10000 ciclos de CPU
             key_counter += cycles
             if key_counter > 10000:
                 # Eventos SDL (entrada por ejemplo)
@@ -109,14 +107,13 @@ class NES(object):
 
                 key_counter = 0
 
-
-            total_cycles += cycles
-
+            total_cycles += cycles              # Incrementamos el contador de ciclos totales
 
             # Estadísticas
             stats_cycles += cycles
 
-            if stats_cycles > 5000:
+            # Hacemos la media de ciclos ejecutados por segundo para fines estadísticos
+            if stats_cycles > 20000:
                 stats_clock = time.time()
 
                 if stats_clock - stats_total_time >= 1:
@@ -167,7 +164,7 @@ file_name = "../roms/Super Mario Bros. (E).nes"
 #file_name = "../tests/instr_test-v4/all_instrs.nes"
 nes = NES(file_name)
 
-nes.run()
+nes.run()               # Ejecutamos
 
 # Código de profiling
 #cProfile.run("nes.run()", "profiling.log")
