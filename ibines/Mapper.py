49c43ebf8dcacd3c4ccb7a8c37e6db90f4c21603
ibon <ibicano@yahoo.es>
2015-06-12 14:30:18 +0200

    Implementado el mapper CNROM y probándolo con el Donkey Kong.

--- ibines/Mapper.py
+++ ibines/Mapper.py
@@ -43,6 +43,8 @@ class Mapper(object):
             return NROM(rom)
         elif mapper_code == 1:
             return MMC1(rom)
+        elif mapper_code == 3:
+            return CNROM(rom)
 
 
 class NROM(Mapper):
@@ -289,6 +291,61 @@ class MMC1(Mapper):
             # Se intercambia el banco 0x8000
             elif prg_swap == 0:
                 self._prg_0 = self._rom.get_prg(bank_number)
+
+
+class CNROM(Mapper):
+
+    MAPPER_CODE = 3
+
+    def __init__(self, rom):
+        super(CNROM, self).__init__(rom)
+
+        #Carga los bancos
+        self._prg_rom_0 = [0x00] * 16384
+        self._prg_rom_1 = [0x00] * 16384
+
+        if self._rom.get_prg_count() == 1:
+            self._prg_rom_0 = self._rom.get_prg(0)
+        elif self._rom.get_prg_count() == 2:
+            self._prg_rom_0 = self._rom.get_prg(0)
+            self._prg_rom_1 = self._rom.get_prg(1)
+
+        self._chr_rom = [0x00] * 8192
+        self._chr_rom = self._rom.get_chr(0)
+
+
+    def read_chr(self, addr):
+        a = addr % 0x4000
+        d = 0x00
+        if 0x0000 <= a < 0x2000:
+            d = self._chr_rom[a]
+
+        return d
+
+
+    def read_prg(self, addr):
+        d = 0x00
+
+        if 0x8000 <= addr < 0xC000:
+            a = (addr - 0x8000) % 0x4000
+            d = self._prg_rom_0[a]
+        elif 0xC000 <= addr < 0x10000:
+            a = (addr - 0x8000) % 0x4000
+            if self._rom.get_prg_count() == 1:
+                d = self._prg_rom_0[a]
+            elif self._rom.get_prg_count() == 2:
+                d = self._prg_rom_1[a]
+
+        return d
+
+
+    def write_prg(self, data, addr):
+        self._chr_rom = self._rom.get_chr(data & 0x03)
+
+
+    def mirror_mode(self):
+        return self._rom.get_mirroring()
+
 
 
 
--- ibines/NES.py
+++ ibines/NES.py
@@ -156,8 +156,8 @@ class NES(object):
 # Inicio del programa
 ###############################################################################
 
-file_name = "../roms/Super Mario Bros. (E).nes"
-#file_name = "../roms/Donkey Kong Classics (USA, Europe).nes"
+#file_name = "../roms/Super Mario Bros. (E).nes"
+file_name = "../roms/Donkey Kong Classics (USA, Europe).nes"
 #file_name = "../tests/nestest.nes"
 #file_name = "../tests/instr_test-v4/rom_singles/10-branches.nes"
 #file_name = "../tests/instr_test-v4/official_only.nes"
